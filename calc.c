c50df090b0e882029719a6018ab3c34d18a3e36d
Kumar Vicky Singh <62302981+kumarvickysingh@users.noreply.github.com>
2020-03-18 00:03:38 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+float f;
+printf(" \n\n\n\n\t\t\tEnter two numbers : ");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("\n\n\t Sum = %d",c);
+d=a*b;
+printf("\n\n\t Mul = %d",d);
+e=a-b;
+printf("\n\n\t Sub = %d",e);
+f=a/b;
+printf("\n\n\t Div = %f",f);
+return(0);
+}
